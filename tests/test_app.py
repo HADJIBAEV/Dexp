f808efdfef92bf76e8b43552f2cd06aee83b4688
Kahramon <kahramonhadjibaev@gmail.com>
2022-01-27 18:32:20 +0300

    Created

--- tests/test_app.py
+++ tests/test_app.py
@@ -6,7 +6,7 @@ from app import app
 class BasicTestCase(unittest.TestCase):
     def test_home(self):
         tester = app.test_client(self)
-        pages = ['/', 'About', 'Contactme', 'Portfolio', 'Services', 'Skills']
+        pages = ['/', 'About', 'Contactme', 'Portfolio', 'Services', 'Skills', 'Qualification']
         for page in pages:
             response = tester.get(page, content_type='html/text')
             self.assertEqual(response.status_code, 200)
